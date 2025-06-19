650add360eee84f7fb7d0e3353b624b6d497559e
Sami Ahmed <samiiiahmed8@gmail.com>
2025-06-19 09:46:42 +0300

    Initial Commit

---
+++ Movie-app/Movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_appApp.swift
+//  Movie-app
+//
+//  Created by Sami on 19/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
